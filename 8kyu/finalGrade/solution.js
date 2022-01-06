be433c5d4093ba7df97ac00eaeb860ed93100b1d
Edgar Acuna <edgaracuna@Edgars-MBP.attlocal.net>
2022-01-05 21:22:01 -0500

    added other optimal solutions from other users for reference

--- 8kyu/finalGrade/solution.js
+++ 8kyu/finalGrade/solution.js
@@ -1,3 +1,4 @@
+//My solution
 function finalGrade (exam, projects) {
     let grade = 0;
     if (exam > 90 || projects > 10) {
@@ -11,3 +12,28 @@ function finalGrade (exam, projects) {
     }
     return grade// final grade
   }
+
+//   -------------------------------------------
+//   Other shorther solutions
+
+// function finalGrade (exam, projects) {
+//     if (exam > 90 || projects > 10) return 100;
+//     else if (exam > 75 && projects >= 5) return 90;
+//     else if (exam > 50 && projects >= 2) return 75;
+//     else return 0;
+//   }
+
+// function finalGrade (exam, projects) {
+//     if(exam > 90 || projects > 10) return 100;
+//     if(exam > 75 & projects >= 5) return 90;
+//     if(exam > 50 & projects >= 2) return 75;
+//     return 0;
+//   }
+
+//   const finalGrade = (exam, projects) => {
+//     return (
+//       exam > 90 || projects > 10 ? 100 :
+//       exam > 75 && projects >= 5 ? 90 :
+//       exam > 50 && projects >= 2 ? 75 : 0
+//     )
+//   }
