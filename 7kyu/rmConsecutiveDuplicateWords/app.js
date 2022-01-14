db19fe37ae7096d365ae3fc2c144d29f05b21e94
Edgar Acuna <acunamatata.ea@gmail.com>
2022-01-14 15:10:21 -0500

    modified app.js to correct code that passed the test

--- 7kyu/rmConsecutiveDuplicateWords/app.js
+++ 7kyu/rmConsecutiveDuplicateWords/app.js
@@ -9,7 +9,7 @@ const removeConsecutiveDuplicates = s => {
     const wordArray = s.split(" ");
 
     for (let i = 0; i < wordArray.length; i++) {
-        if (!newString.includes(wordArray[i])) {
+        if (wordArray[i] !== wordArray[i + 1]) {
             newString += wordArray[i] + ' ';
         }
     }
@@ -18,3 +18,11 @@ const removeConsecutiveDuplicates = s => {
 }
 
 console.log(removeConsecutiveDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
+
+// ----------------
+// Other Solutions
+// ----------------
+
+// const removeConsecutiveDuplicates = s => s.split(" ").filter((x,i,arr) => x!=arr[i-1]).join(" ");
+
+// const removeConsecutiveDuplicates = s => s.replace(/\b(\w+)(?: \1)+\b/g, '$1')
